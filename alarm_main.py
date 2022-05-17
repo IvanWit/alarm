2b70835fd91521f1484419a5625a68927aedfeab
IvanWit <104214765+IvanWit@users.noreply.github.com>
2022-05-17 23:48:16 +0200

    Update alarm_main.py

--- alarm_main.py
+++ alarm_main.py
@@ -1,6 +1,9 @@
+from sqlite3 import Time
 from tkinter import*
 from datetime import datetime
 import datetime
+from tokenize import String
+from turtle import numinput
 from pydub import AudioSegment
 from pydub.playback import _play_with_simpleaudio
 import random
@@ -8,24 +11,47 @@ import time
 
 root = Tk()
 root.title("Budík")
-root.geometry("400x200")
+root.geometry("400x250")
 
-
-def set():
-	global set_alarm_time
-	user = int(hour.get())*3600+ int(min.get())*60 + int(sec.get())
-	cas = datetime.datetime.now()
-	curr = int(cas.hour)*3600 + int(cas.minute)*60 + int(cas.second)
-	if (user - curr) < 0:
-		set_alarm_time = (86400 - abs(user - curr))
-	else :
-		set_alarm_time = abs(user - curr)
-	seznam()
 waiting = []
 run_once = 0
+prnt = StringVar()
+display = []
+def set():
+	global set_alarm_time
+	global nhour
+	global nmin
+	global nsec
+	nhour = int(str(hour.get())[:2])
+	nmin = int(str(min.get())[:2])
+	nsec = int(str(sec.get())[:2])
+	if nhour >= 24:
+		warning()
+	elif nmin >= 60:
+		warning()
+	elif nsec >= 60:
+		warning()
+	else:
+		user = int(nhour)*3600 + int(nmin)*60 + int(nsec)
+		cas = datetime.datetime.now()
+		curr = int(cas.hour)*3600 + int(cas.minute)*60 + int(cas.second)
+		if (user - curr) < 0:
+			set_alarm_time = (86400 - abs(user - curr))
+		else :
+			set_alarm_time = abs(user - curr)
+		seznam()
+
+def warning():
+	war = Toplevel(root)
+	war.geometry("600x75")
+	war.title("POZOR!")
+	Label(war, text="Chybné zadání času!", font=("helvetica 40 bold"), fg="red", anchor=CENTER).pack()
 def seznam():
 	global waiting
 	global run_once
+	set_u = "{:02d}:{:02d}:{:02d}".format(nhour, nmin, nsec)
+	display.append(set_u)
+	prnt.set(", ".join(display))
 	waiting.append(set_alarm_time)
 	if run_once == 0:
 		run_once = run_once + 1
@@ -34,38 +60,45 @@ def seznam():
 			pass
 
 
-def alarm():
+def alarm():	
+
 	if waiting:
 		for i in range(len(waiting)):
+			if i >= len(waiting):
+				i = 0
+			elif i < 0:
+				i = 0
 			waiting[i] = waiting[i] - 1
-			print (waiting)
+			print (waiting, i)
+			if waiting[i] <= 0:
+				waiting.remove(waiting[i])
+				print("Budíček")
+				sound()
 		root.after(1000, alarm)
-		if 0 in waiting:
-			waiting.remove(0)
-			print("Budíček")
-			zvuk()
-			
 	else:
 		print("Budíček")
-		zvuk()
+		sound()
 		exit()
 			
-def zvuk():
-	zvuk1 = AudioSegment.from_mp3("C:/Users/42060/Desktop/pgs/a.mp3")
-	zvuk2 = AudioSegment.from_mp3("C:/Users/42060/Desktop/pgs/b.mp3")
-	zvuk3 = AudioSegment.from_mp3("C:/Users/42060/Desktop/pgs/c.mp3")
-	zvuk4 = AudioSegment.from_mp3("C:/Users/42060/Desktop/pgs/d.mp3")
-	zvuk5 = AudioSegment.from_mp3("C:/Users/42060/Desktop/pgs/e.mp3")
-	zvuky = [zvuk1, zvuk2, zvuk3, zvuk4, zvuk5]
-	pogzvuk = random.choice(zvuky)
-	x = _play_with_simpleaudio(pogzvuk)
+def sound():
+	sound1 = AudioSegment.from_mp3("a.mp3")
+	sound2 = AudioSegment.from_mp3("b.mp3")
+	sound3 = AudioSegment.from_mp3("c.mp3")
+	sound4 = AudioSegment.from_mp3("d.mp3")
+	sound5 = AudioSegment.from_mp3("e.mp3")
+	sounds = [sound1, sound2, sound3, sound4, sound5]
+	pogsound = random.choice(sounds)
+	x = _play_with_simpleaudio(pogsound)
+	for z in range(len(waiting)):
+		waiting[z] = waiting[z] - 15
 	time.sleep(15)
 	x.stop()
 	if not waiting:
 		exit()
 
-Label(root,text="Budík",font=("Helvetica 20 bold"),fg="red").pack(pady=10)
-Label(root,text="Zadejte čas",font=("Helvetica 15 bold")).pack(pady=15)
+Label(root, text="Budík", font=("Helvetica 20 bold"), fg="red").pack(pady=10)
+Label(root, text="Zadejte čas", font=("Helvetica 15 bold")).pack(pady=10)
+Label(root, textvariable=prnt, font=("Helvetice 10"), wraplengt=300).pack(pady=5)
 
 frame = Frame(root)
 frame.pack()
@@ -74,9 +107,10 @@ hour = IntVar()
 min = IntVar()
 sec = IntVar()
 
-hourTime= Entry(root,textvariable = hour,bg = "pink",width = 15).place(x=110,y=45)
-minTime= Entry(root,textvariable = min,bg = "pink",width = 15).place(x=150,y=45)
-secTime = Entry(root,textvariable = sec,bg = "pink",width = 15).place(x=200,y=45)
+
+hourTime= Entry(root, textvariable=hour, bg="pink", width=15).place(x=110,y=45)
+minTime= Entry(root, textvariable=min, bg="pink", width=15).place(x=150,y=45)
+secTime = Entry(root, textvariable=sec, bg="pink", width=15).place(x=200,y=45)
 
 tlacitko = Button(root,text="Zapnout budík",font=("Helvetica 15"), command=set)
 tlacitko.pack(pady=5)
